fd6296a778ad2aef4e43d70cef6881ecf3c0aa1a
Zrinko Vukšić <zrinko.vuksic@king-ict.cloud>
2024-11-19 14:47:32 +0100

    Add  blazor notifications to BaseHttpClient

--- BlazorServer.UI/SharedServices/BaseHttpClient.cs
+++ BlazorServer.UI/SharedServices/BaseHttpClient.cs
@@ -2,16 +2,18 @@
 using Microsoft.Extensions.Localization;
 using BlazorServer.UI.Properties;
 using Radzen;
+using Newtonsoft.Json.Linq;
+using System.Linq.Dynamic.Core;
 
 namespace BlazorServer.UI.SharedServices
 {
     public class BaseHttpClient : IBaseHttpClient
     {
-        protected readonly IHttpClientFactory _httpClientFactory;      
+        protected readonly IHttpClientFactory _httpClientFactory;
         protected readonly IHttpContextAccessor _httpContextAccessor;
         protected readonly NavigationManager _navigationManager;
         protected readonly NotificationService _notificationService;
-        public readonly HttpClient _httpClient;        
+        public readonly HttpClient _httpClient;
         private readonly IStringLocalizer _localizer;
 
         public BaseHttpClient(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor, NavigationManager navigationManager, NotificationService notificationService, IStringLocalizer<Resources> localizer)
@@ -27,7 +29,7 @@ namespace BlazorServer.UI.SharedServices
         public string GetToken()
         {
             if (_httpContextAccessor.HttpContext?.User.Claims.Any() ?? false)
-                return _httpContextAccessor.HttpContext.User.Claims.First(c => c.Type == "jwtToken").ToString().Split(":")[1].Trim();            
+                return _httpContextAccessor.HttpContext.User.Claims.First(c => c.Type == "jwtToken").ToString().Split(":")[1].Trim();
             else
                 return String.Empty;
         }
@@ -41,21 +43,16 @@ namespace BlazorServer.UI.SharedServices
 
             if (response.IsSuccessStatusCode)
             {
-                return await response.Content.ReadAsStringAsync();            
+                return await response.Content.ReadAsStringAsync();
             }
             else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
             {
-                _notificationService.Notify(new NotificationMessage
-                {
-                    Severity = NotificationSeverity.Warning,
-                    Duration = 10000,
-                    Summary = _localizer["error401"]
-                });
+                ShowNotification(NotificationSeverity.Warning, _localizer["error401"]);
 
                 _navigationManager.NavigateTo("RedirectToLogin/false"); //or RedirectToLogin/true
 
                 return "You are not logged in/Cookie has expired";
-            }    
+            }
             else
             {
                 var res = response.Content.ReadAsStringAsync();
@@ -68,13 +65,47 @@ namespace BlazorServer.UI.SharedServices
             if (_httpClient.DefaultRequestHeaders.Authorization == null)
                 _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", GetToken());
 
-            var result = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress + endpoint, model);
-            if (result.IsSuccessStatusCode)
+            var response = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress + endpoint, model);
+            if (response.IsSuccessStatusCode)
             {
-                return await result.Content.ReadAsStringAsync();            
+                var resultMessage = await response.Content.ReadAsStringAsync();
+                ShowNotification(NotificationSeverity.Success, resultMessage);
+
+                return resultMessage;
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var detailsErrors = await response.Content.ReadAsStringAsync();
+                
+                try
+                {
+                    JToken token = JObject.Parse(detailsErrors)["errors"];
+                    foreach (var item in token.Children())
+                    {
+                        ShowNotification(NotificationSeverity.Warning, item.Values().FirstOrDefault().ToString());
+                    }
+                }
+                catch
+                {
+                    ShowNotification(NotificationSeverity.Warning, detailsErrors); //_localizer["error400"]                    
+                }
+            }
+            else
+            {
+                ShowNotification(NotificationSeverity.Error, response.ReasonPhrase);
             }
 
-            return result.StatusCode.ToString();
+            return response.StatusCode.ToString();
+        }
+
+        private void ShowNotification(NotificationSeverity notificationSeverity, string? summaryMessage)
+        {
+            _notificationService.Notify(new NotificationMessage
+            {
+                Severity = notificationSeverity,
+                Duration = 10000,
+                Summary = _localizer[summaryMessage]
+            });
         }
     }
 }
